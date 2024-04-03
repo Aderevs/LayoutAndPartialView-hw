fff6bfd8e7ac1ad2717ace900b26a2fc00ade5ad
Aderevs <aderevs@gmail.com>
2024-04-03 17:09:05 +0300

    task 3

---
+++ Task2-3/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Task2_3.Models;
+
+namespace Task2_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
