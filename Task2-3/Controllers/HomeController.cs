091a90bef444ea51ededbb64db0f77ca5a2a919e
Aderevs <aderevs@gmail.com>
2024-04-04 12:41:43 +0300

    Task 2

--- Task2-3/Controllers/HomeController.cs
+++ Task2-3/Controllers/HomeController.cs
@@ -19,6 +19,18 @@ namespace Task2_3.Controllers
         {
             return View();
         }
+        public IActionResult Menu1()
+        {
+            return View();
+        }
+        public IActionResult Menu2()
+        {
+            return View();
+        }
+        public IActionResult Menu3()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
